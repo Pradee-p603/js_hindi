25d675e351f55cbf31de4ce8e1892d5a50f56609
Pradee-p603 <130845021+Pradee-p603@users.noreply.github.com>
2024-09-13 14:11:48 +0000

    variable done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail ="pradeepptel57543@gmail.com"
+var accountPassword ="1234"
+accountCity ="bhopal"
+let accountState;
+//if we do not declare any value in let keyword then its is by default undefine
+
+//accountId = 2 not allowed
+accountEmail = "mangalpatel@gmail.com"
+accountPassword = "999999"
+accountCity = "sehore"
+
+console.log(accountEmail)
+
+/*
+prefer not to use var 
+because of issue in blog scope and functional scope
+
+*/
+console.table([accountId,accountEmail,accountPassword,,accountEmail,accountState])
