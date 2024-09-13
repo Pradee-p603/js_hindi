1409f9c947c57ce7ceaf3e09f95652aa39113c3b
Pradee-p603 <130845021+Pradee-p603@users.noreply.github.com>
2024-09-13 14:36:22 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer  version
+
+//alert(3 + 3) // we are using not js not browser so it will give exception not define
+
+console.log(3+
+    3) //code readablity should be high must be in proper format
+console.log("pradeep")
+
+let name = "pradeep" // string
+let age = 18 //number
+let isLoggedIn = true //boolean
+
+//primitive data types
+
+// number => 2 to power of 53
+//bigint => for big number
+//string => "", '' both single and double qouts define string
+// boolean => true or false
+// null => standalone value
+//undefine => when value is not assign to varaiable
+//symbol => unique
+
+//object
+console.log(typeof undefined) // output undefine
+console.log(typeof null) // output object
