9a4d2ea7cdf014570c04a5a74918996dab1740d6
Pradee-p603 <130845021+Pradee-p603@users.noreply.github.com>
2024-09-14 19:23:58 +0000

    comparison summary

---
+++ 01_basics/dataTypes.summary.js
@@ -0,0 +1,49 @@
+// # primitive data types   
+// it can be call by value
+// 7 types : string, Number, Boolean, null, undefined, Symbol, BigInt
+
+//can javaScript be statically typed or dynamically?
+//ans = avaScript is dynamically typed. 
+//This means Variables can hold any type of value and can change types at runtime without any type declaration
+//example
+const score = 100;
+const scoreValue = 100.5
+
+const isLoggedIn = true
+const outsideTemp = null
+
+let userEmail; 
+
+//symbol
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId )
+
+const bigNumber = 2381282212121486n
+
+
+
+
+
+
+
+// # refrence data types(non primitive)
+// arrays, objects, Function
+
+const heros = ["shaktimaan", "Nagraj", "doga"] // array
+
+let myObject = {
+name: "pradeep",
+age: 22
+
+
+}
+//function
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof myFunction);
